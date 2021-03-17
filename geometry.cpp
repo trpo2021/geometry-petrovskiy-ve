7ec49cf5c7e79a46f7294c078e1a0dad3a45424f
petrovskiy-ve <ip014s24@bwp05.csc.local>
2021-03-17 18:47:55 +0700

    Started over geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <iostream>
+using namespace std;
 
 float const Pi = 3.1415;
 
+void insert_check()
+{
+	string obj;
+	char array[20]
+	
+	cin >> obj;
+	
+	cout << obj;
+}
+
 int main()
 {
-  float x = 0, y = 0, r = 0, P = 0, S = 0;
-
-  printf("Insert coordinates of center and radius of circle, line by line\n");
-  scanf("%f", &x);
-  scanf("%f", &y);
-  scanf("%f", &r);
-
-  P = 2 * Pi * r;
-  S = Pi * r * r;
-
-  printf("P = %f\n", P);
-  printf("S = %f\n", S);
+  insert_check();
 }
